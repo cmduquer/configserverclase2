760394466756c98ded84bad4186226beae329bdb
cmduquer <cmduquer@192.168.1.15>
2019-08-03 12:36:52 -0500

    https://github.com/cmduquer/configserverclase2.git

---
+++ src/main/java/com/microservicios2/clase2/configserver/CloudConfigServerClase2Application.java
@@ -0,0 +1,15 @@
+package com.microservicios2.clase2.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigServerClase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerClase2Application.class, args);
+	}
+
+}
